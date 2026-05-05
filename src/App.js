a8ff358b179aee2fbc393c91a4ac444fd75a2d5d
leo-Ito007 <leonardo.salles.felipe@escola.pr.gov.br>
2026-05-05 12:29:58 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import Header from "./componentes/Header"
+import Main from "./componentes/Main"
+import Footer from "./componentes/Footer"
+
 import logo from './logo.svg';
 import './App.css';
 
---
+++ src/componentes/Header.js
@@ -0,0 +1,20 @@
+function Header() {
+
+    return (
+        <header>
+            <div class="headerTopo">
+                <h1>Curso Técinco em Desenvolvimento de Sistema</h1>
+            </div>
+            <nav class="headerMenu">
+                <ul>
+                    <li>Home</li>
+                    <li>Curso</li>
+                    <li>Dsciplinas Básicas</li>
+                    <li>Dsciplinas Técincas</li>
+                    <li>Equipe</li>
+                </ul>
+            </nav>
+            </header>
+    )
+}
+export default Header;
